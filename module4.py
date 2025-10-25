f0a099fcbdcf7ecd1f591ce2a1467253b65cdb3e
MarinaLyu <167391964+MarinaLyu@users.noreply.github.com>
2025-10-24 22:32:24 -0700

    Update module4.py

--- module4.py
+++ module4.py
@@ -1,3 +1,8 @@
+# The program asks the user for input N (positive integer) and reads it.
+# Then the program asks the user to provide N numbers (one by one) and reads all of them (again, one by one)
+# In the end, the program asks the user for input X (integer) and outputs: "-1" if there were no such X among N read numbers, or the index (from 1 to N) of this X if the user inputed it before.
+# Hint: use input() function
+
 N = int(input("Enter a positive integer N: "))
 numbers = []
 for i in range(N):
@@ -8,3 +13,4 @@ if X in numbers:
     print(numbers.index(X) + 1)
 else:
     print(-1)
+
